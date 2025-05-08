2c71f71776b40d7d56925b8805bbec73c9b5cf33
Felipe <126116835+Felipe7771@users.noreply.github.com>
2025-05-08 14:54:26 -0300

    ISA PHP 1.0.2

--- ISA/php_functs/button_login.php
+++ ISA/php_functs/button_login.php
@@ -1,17 +1,37 @@
 <?php
-
+session_start();
 function Check_login(){
     # checagem se a variável IsLogin existe e é verdadeira
-    if( isset($IsLogin) && $IsLogin == true) {
-        # significa que o usuário está logado
-        # mudar botão para LOGOUT
-        return true;
+    if( isset($_SESSION['IsLogin']) ) {
+
+        # checagem se a variável é verdadeira
+        if ($_SESSION['IsLogin']){
+            # significa que o usuário está logado
+            # mudar botão para LOGOUT
+            return true;
+        }else{
+
+            # significa que o usuário não está logado
+            return false;
+        }
+        
     } else {
         # significa que o usuário não está logado
-        # mudar botão para LOGIN
         return false;
     }
 }
 
+function Define_button_log($is_login){
+    # checagem se o usuário está logado
+    if($is_login){
+        # SIM, está logado
+        # definir botão LOGOUT
+        return '<button class= "butlogout"><i class="bx bx-user icon-log"></i>Logout</button>';
+    } else {
 
+        # NÃO, está desconectado
+        # definir botão LOGIN
+        return '<button class= "butlog"><i class="bx bx-user icon-log"></i>Login</button>';
+    }
+}
 ?>
