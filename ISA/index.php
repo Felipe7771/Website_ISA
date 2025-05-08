f076840f2273fbeb4260f7e1dc6ae800e2194622
Felipe <126116835+Felipe7771@users.noreply.github.com>
2025-05-08 13:19:01 -0300

    ISA HOME 1.0.2

--- ISA/index.php
+++ ISA/index.php
@@ -1,27 +1,28 @@
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ISA</title>
+  <title>Bem-Vindo</title>
+  <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@700&display=swap" rel="stylesheet">
-  <link rel="icon" href="img\icon_nav.png">
-  <link rel="stylesheet" href="css\style_home.css">
+  <link rel="icon" href="img/icon_nav.png">
 </head>
 <body>
 
   <header>
-    <img src="https://png.pngtree.com/png-vector/20220721/ourmid/pngtree-parrot-logo-vector-illustration-png-image_5454873.png" alt="Logo da empresa">
+    <img src='img/logo_bar.png'>
     <div>
-      <span class="icon" title="Pesquisar">🔍</span>
-      <span class="icon" title="Configurações">⚙️</span>
+      <i class='bx bx-home-alt icon-style'></i>
+      <i class='bx bxs-group icon-style'></i>
+      <button class= "butlog"><i class='bx bx-user icon-log'></i>Login</button>
+      
     </div>
   </header>
 
   <main id="mainContainer">
     <section class="left-panel">
-      <h1 class= "h1">Bem-vindo</h1>
+      <h1 class= "h1">BEM VINDO</h1>
       <p class= "p">Tenha uma ótima experiência com nossa nova IA por interação de voz.</p>
       <button class= "button">Converse com a ISA</button>
     </section>
